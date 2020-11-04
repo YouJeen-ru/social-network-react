f8d9cffa07fd7b72ace1f14ab5e30258e21d6cd5
YouJeen-ru <1.rouch@mail.ru>
2020-11-04 16:02:58 +0300

    profile-reducer refactor

--- src/Redux/profile-reducer.tsx
+++ src/Redux/profile-reducer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
@@ -16,28 +16,28 @@ const ProfileReducer = (state = initialState, action: any) => {
 
 
     switch (action.type) {
-        case ADD_POST: {
+        case ADD_POST:
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             }
-            let stateCopy = {...state}
-            stateCopy.posts = [...state.posts]
-            stateCopy.posts.push(newPost)
-            stateCopy.newPostText = ''
-            return stateCopy;
-    }
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            }
+
+
         case UPDATE_NEW_POST_TEXT:
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.newText
-            return stateCopy;
+            return {
+                ...state,
+                newPostText: action.newText
+            }
 
         default:
             return state
     }
-
-
 
 
 };
